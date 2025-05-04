fe7036777d7b74fc63408ef2b3504f14329d6325
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-05-04 14:58:28 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/app/(components)/commons/Header";
 import HeroSection from "@/app/(components)/landing/HeroSection";
+import HeroSection2 from "@/app/(components)/landing/HeroSection2";
 import FeaturesSection from "@/app/(components)/landing/FeaturesSection";
 import CTASection from "@/app/(components)/landing/CTASection";
 import TestimonialsSection from "@/app/(components)/landing/TestimonialsSection";
@@ -11,7 +12,8 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
       <Header />
-      <HeroSection />
+      {/* <HeroSection /> */}
+      <HeroSection2 />
       <FeaturesSection />
       <TestimonialsSection />
       <CTASection />
