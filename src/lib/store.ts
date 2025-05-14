0942870d1f61292fc77d594a42329a95604155b9
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-05-14 23:48:41 +0600

    up

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -26,3 +26,16 @@ export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+//fffffffffffffff
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./slices/authSlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: authReducer,
+//   },
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
