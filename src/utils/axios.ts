5b35541ba199aa546f71fc54ff5c412fa31a4131
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-05-20 22:14:56 +0600

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,16 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+// const instance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+// });
 
-instance.interceptors.request.use((config) => {
-  const token =
-    typeof window !== "undefined" ? localStorage.getItem("token") : null;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// instance.interceptors.request.use((config) => {
+//   const token =
+//     typeof window !== "undefined" ? localStorage.getItem("token") : null;
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
-export default instance;
+// export default instance;
