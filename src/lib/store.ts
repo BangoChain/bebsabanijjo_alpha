800723a26acd8dc355500a2b561c88d60a0569a7
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2025-05-20 22:18:43 +0600

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,33 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import authReducer from "@/features/auth/authSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers } from "redux";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import authReducer from "@/features/auth/authSlice";
 
-const rootReducer = combineReducers({
-  auth: authReducer,
-});
+// const rootReducer = combineReducers({
+//   auth: authReducer,
+// });
 
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["auth"],
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["auth"],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }),
-});
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({ serializableCheck: false }),
+// });
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
 
-//fffffffffffffff
+//old one
 // import { configureStore } from "@reduxjs/toolkit";
 // import authReducer from "./slices/authSlice";
 
