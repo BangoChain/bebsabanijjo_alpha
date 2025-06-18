6a7c950ccf4e3ff28a1238770d1a72326a0ae14e
bangochain <query.bangochain@gmail.com>
2025-06-18 18:30:07 +0600

    flow commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Header from "@/app/(components)/commons/Header";
 import HeroSection from "@/app/(components)/landing/HeroSection";
-// import HeroSection2 from "@/app/(components)/landing/HeroSection2";
 import FeaturesSection from "@/app/(components)/landing/FeaturesSection";
 import CTASection from "@/app/(components)/landing/CTASection";
 import TestimonialsSection from "@/app/(components)/landing/TestimonialsSection";
 import PricingPlans from "@/app/(components)/landing/PricingSection";
-// import PricingPlans2 from "@/app/(components)/landing/PricingSection2";
 import Footer from "@/app/(components)/commons/Footer";
 
 export default function Home() {
